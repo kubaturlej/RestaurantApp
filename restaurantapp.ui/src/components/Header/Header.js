b343dfd02a16f2a5cf186324bac05b4b47df6daf
kubinio <kuba.turlej@wp.pl>
2023-05-24 19:27:36 +0200

    client setup

---
+++ restaurantapp.ui/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+        <div>
+        <h1>Moja Strona</h1>
+        <p>Witaj na mojej stronie!</p>
+        </div>
+  
+    </header>
+  )
+}
+
+export default Header
---
+++ restaurantapp.ui/src/components/Restaurants/Restaurants.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Restaurant from './Restaurant/Restaurant'
+
+const Restaurants = () => {
+  return (
+    <>
+        <Restaurant/>
+        <Restaurant/>
+        <Restaurant/>
+    </>
+  )
+}
+
+export default Restaurants
